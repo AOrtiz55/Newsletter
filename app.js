ff0d380ffdccfbf9193b2cb98fb9bc2ff89113b4
Aaron Ortiz <90681165+AOrtiz55@users.noreply.github.com>
2021-11-04 22:54:39 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,71 @@
+const express = require("express");
+const bodyParse = require("body-parser");
+const request = require("request");
+const https=require("https");
+const app = express();
+app.use(express.static("public"));//have to create a folder. public/css. and put your styles in css and images folder in there
+app.use(bodyParse.urlencoded({extended:true}));
+
+
+app.get("/", function(req,res){
+
+res.sendFile(__dirname+"/signup.html");
+});
+app.post("/", function(req, res){
+  const firstName = req.body.fName;
+  const lastName = req.body.lName;
+  const email = req.body.email;
+
+  const data = {
+    members: [
+      {
+      email_address:email,
+      status:"subscribed",
+      merge_fields:{
+        FNAME:firstName,
+        LNAME: lastName
+      }
+    }
+  ]
+};
+const jsonData =JSON.stringify(data);
+
+const url ="https://us20.api.mailchimp.com/3.0/lists/cc45ceb386";
+const options = {
+  method: "POST",
+  auth:"aaron1:7ba94ed2452166987f6aac65c5ceaffb-us20"
+}
+
+const request= https.request(url,options,function(response){
+
+if (response.statusCode===200){
+  res.sendFile(__dirname+"/success.html");
+}else{
+  res.sendFile(__dirname+"/failure.html");
+}
+
+  response.on("data", function(data){
+    console.log(JSON.parse(data));
+
+  });
+
+});
+
+
+request.write(jsonData);
+request.end();
+});
+
+app.post("/failure", function(req, res){
+  res.redirect("/");
+});
+
+
+app.listen(process.env.PORT|| 3000, function(){
+  console.log("server on 3000");
+});
+
+//7ba94ed2452166987f6aac65c5ceaffb-us20
+
+//list id
+//cc45ceb386
